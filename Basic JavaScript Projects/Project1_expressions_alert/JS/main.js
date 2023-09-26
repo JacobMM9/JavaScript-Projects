52c5407093b28a4e05899491d9c7843baacf3597
Jacob May <jacobmm.softwaredev@gmail.com>
2023-09-25 23:35:25 -0500

    added new sections to min.js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,23 @@
+document.write('Hello, World!');
+
+var A = " This is a string";
+
+document.write(A);
+
+var SEPT = " september";
+
+window.alert(SEPT);
+
+document.write("<br>\"THIS\" is how you add quotation marks for mark\'s page");
+
+document.write("<br>This is a concatination of this and the next line..." + 
+" idk why you want to do this but here you go. " + "-\" jake\"<br>");
+
+var B = "Concatenated" + " String";
+document.write(B);
+
+var food = "pizza", color = "orange", Name = "Jakey";
+
+document.write(Name);
+
+document.write(4 + 4);
