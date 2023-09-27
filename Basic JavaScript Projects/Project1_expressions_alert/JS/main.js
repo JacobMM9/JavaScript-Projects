db7501781c624834367ba05566a2cc3d9855a466
Jacob May <jacobmm.softwaredev@gmail.com>
2023-09-27 11:04:28 -0500

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,23 +1,28 @@
-document.write('Hello, World!');
+document.write('Hello, World!'); //this writes hello world in the html doc
 
-var A = " This is a string";
+var A = " This is a string"; //creates a string
 
-document.write(A);
+document.write(A); // displays var A
 
-var SEPT = " september";
+var SEPT = " september"; 
 
 window.alert(SEPT);
 
-document.write("<br>\"THIS\" is how you add quotation marks for mark\'s page");
+document.write("<br>\"THIS\" is how you add quotation marks for mark\'s page"); //showing how to add "" in doc
 
 document.write("<br>This is a concatination of this and the next line..." + 
-" idk why you want to do this but here you go. " + "-\" jake\"<br>");
+" idk why you want to do this but here you go. " + "-\" jake\"<br>");   // showing off concatenation using + 
 
 var B = "Concatenated" + " String";
 document.write(B);
 
-var food = "pizza", color = "orange", Name = "Jakey";
+var food = "pizza", color = "orange", Name = "Jakey"; // showing you can make a bunch of variables together
 
 document.write(Name);
 
 document.write(4 + 4);
+
+var Sent1("This is the beginning of the string");
+
+var Sent2("and this is the end of the string");
+
