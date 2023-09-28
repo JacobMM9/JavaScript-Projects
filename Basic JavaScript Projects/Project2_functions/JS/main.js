ed903c637642607bf92453fbac6e6aeaf41f8eca
Jacob May <jacobmm.softwaredev@gmail.com>
2023-09-28 16:37:37 -0500

    added a bunch of new function
    tweaked the functions and tested them to learn the format

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,18 @@
+function myStyle(){
+    document.getElementById("H1style").style.color = "green"
+    document.getElementById("H2style").style.color = "red";
+    myStyle = document.getElementById("demo");
+    myStyle.style.color= "orange";
+}
+
+function sum(a, b) {
+    console.log(a + b)
+}
+
+sum(1, 2)
+
+function myFunction() {
+    var sentence= "Im learning codeing";
+    sentence += " to become a software dev";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
